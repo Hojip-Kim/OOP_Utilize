f1c5a84606832c08381764fd96fb818fefb553d0
hojipkim <rlaghwlq12@gmail.com>
2022-10-18 20:45:07 +0900

    commit

--- src/JAVA_OOP_util1/sam04/a3_Upcasting/교직원.java
+++ src/JAVA_OOP_util1/sam04/a3_Upcasting/교직원.java
@@ -4,6 +4,9 @@ public class 교직원 extends 사람{
     public String 학교명;
     public long 번호; // 사원번호(학교의 교직원번호)
 
+    public 교직원(){
+
+    }
     public 교직원(String 이름, long 번호, String 학교명, long 교원번호){
         super.이름 = 이름;
         super.번호 = 번호;
